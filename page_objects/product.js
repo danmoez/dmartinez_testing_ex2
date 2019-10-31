edd8c5c0519a09e6b3c71b5d3af334e7ad23a48a
Daniel Martinez <dmorua1791@gmail.com>
2019-10-30 22:16:36 -0600

    3rd Test Case

--- page_objects/product.js
+++ page_objects/product.js
@@ -4,6 +4,8 @@ module.exports = {
     elements: {
         product_qty: '.qty',
         cart_contents: '.cart-contents',
+        coupon_code: '#coupon_code',
+        apply_coupon_button: "//div[@class='coupon']/button",
     },
     commands: [{
         setProductQty(productQty) {
@@ -14,6 +16,16 @@ module.exports = {
         goToCart() {
             return this
                 .click('@cart_contents');
+        },
+        setCouponCode(coupon_name) {
+            return this
+                .setValue('@coupon_code', coupon_name);
+        },
+        clickApplyButton() {
+            return this
+                .useXpath()
+                .click(`//div[@class='coupon']/button`)
+                .useCss();
         },
         deleteViaAPI() {        
             var headers = {
@@ -31,7 +43,7 @@ module.exports = {
             };
 
             var options = {
-            url: 'http://34.205.174.166/wp-json/wc/v3/products/277?consumer_key=consumer_secret&force=true',
+            url: 'http://34.205.174.166/wp-json/wc/v3/products/292?consumer_key=consumer_secret&force=true',
             method: 'DELETE',
             headers: headers
             };
@@ -67,6 +79,69 @@ module.exports = {
         }
 
         request(options, callback);
+
+        },
+
+        createCuponViaAPI() {
+            var headers = {
+            'Accept': '*/*',
+            'Authorization': 'Basic c2hvcG1hbmFnZXI6RHV1eCBQSUVkIGVVbEsgTG1pbiBjUjNjIDVpMWg=',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Host': '34.205.174.166',
+            'Postman-Token': 'a41b4231-2c42-4cba-bff1-4d52c2ec49ee,b3c29ebf-eabd-49c4-8110-65b2baf2aa64',
+            'User-Agent': 'PostmanRuntime/7.11.0',
+            'accept-encoding': 'gzip, deflate',
+            'cache-control': 'no-cache',
+            'content-length': ''
+            };
+
+            var options = {
+                url: 'http://34.205.174.166/wp-json/wc/v3/coupons?code=100off_daniel&discount_type=percent&amount=100&minimum_amount=30',
+                method: 'POST',
+                headers: headers
+            };
+
+            function callback(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                }
+            }
+
+            request(options, callback);
+
+        },
+
+        deleteCuponViaAPI() {
+            var headers = {
+            'Accept': '*/*',
+            'Authorization': 'Basic c2hvcG1hbmFnZXI6RHV1eCBQSUVkIGVVbEsgTG1pbiBjUjNjIDVpMWg=',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Host': '34.205.174.166',
+            'Postman-Token': 'a6716b2f-46d2-43f0-bcc0-e33f0d6f6fb4,02ad499e-ea85-4581-9a85-6c60e74de7d6',
+            'User-Agent': 'PostmanRuntime/7.11.0',
+            'accept-encoding': 'gzip, deflate',
+            'cache-control': 'no-cache',
+            'content-length': ''
+            };
+
+            var options = {
+                url: 'http://34.205.174.166/wp-json/wc/v3/coupons/279?force=true',
+                method: 'DELETE',
+                headers: headers
+            };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
+        }
+
+        request(options, callback);
+
 
         },
     }]
--- tests/test_3.js
+++ tests/test_3.js
@@ -10,74 +10,74 @@ module.exports = {
     var product_url= '/product/dmartinez/';
     var productQty= '7';
     var cart_url = 'cart';
-    // var headers = {
-    // 'Authorization': 'Basic c2hvcG1hbmFnZXI6RHV1eCBQSUVkIGVVbEsgTG1pbiBjUjNjIDVpMWg=',
-    // 'Content-Type': 'application/json',
-    // 'Postman-Token': 'd2b63910-a940-4e2e-9c18-673d3fd28388',
-    // 'cache-control': 'no-cache'
-    // };
-
-    // var options = {
-    // url: 'http://34.205.174.166/wp-json/wc/v3/products?consumer_key=consumer_secret&name=dmartinez&type=simple&regular_price=5&description=Sample&short_description=Testing',
-    // method: 'POST',
-    // headers: headers
-    // };
-
-    // function callback(error, response, body) {
-    //     if (!error && response.statusCode == 200) {
-    //         console.log(body);
-    //     }
-    // }
-
-    // before: request(options, callback);
+    var coupon_name = '100off_daniel';
 
     product_page
         .createViaAPI();
+    product_page
+        .createCuponViaAPI();
+    client.refresh();
 
-    // page
-    //     .navigate()
+    page
+        .navigate()
 
-    // const mainPageHeader = `#masthead`;
-    // client.assert.visible(mainPageHeader, 'UI: Main Page Visible');
+    const mainPageHeader = `#masthead`;
+    client.assert.visible(mainPageHeader, 'UI: Main Page Visible');
 
-    // client
-    //     .url("http://34.205.174.166"+product_url);
+    client
+        .url("http://34.205.174.166"+product_url);
 
-    // const product_page_ui = `//*[contains(@class, 'product-type-simple')]`;
-    // client.useXpath();
-    // client.assert.visible(product_page_ui, 'UI: Product Page is Visible');
-    // client.useCss();
+    const product_page_ui = `//*[contains(@class, 'product-type-simple')]`;
+    client.useXpath();
+    client.assert.visible(product_page_ui, 'UI: Product Page is Visible');
+    client.useCss();
 
-    // product_page
-    //     .setProductQty(productQty);
+    product_page
+        .setProductQty(productQty);
 
-    // client
-    //     .keys(client.Keys.ENTER);
+    client
+        .keys(client.Keys.ENTER);
     
-    // const cart_qty = `//a[@class='cart-contents']/span[@class='count']`;
-    // client.useXpath();
-    // client.assert.containsText(cart_qty, productQty, 'UI: Cart Quantity Updated');
-    // client.useCss();
 
-    // product_page
-    //     .goToCart();
+    product_page
+        .goToCart();
 
-    // client.assert.urlContains(cart_url, 'Params: URL of Cart is Displayed');
+    client.assert.urlContains(cart_url, 'Params: URL of Cart is Displayed');
 
-    // const product_cart = `//td[@class='product-name']/a`;
-    // client.useXpath();
-    // client.assert.containsText(product_cart, product, 'UI: Product in Cart visible');
-    // client.useCss();
+    const product_cart = `//td[@class='product-name']/a`;
+    client.useXpath();
+    client.assert.containsText(product_cart, product, 'UI: Product in Cart visible');
+    client.useCss();
     client.pause(9000);
 
+    product_page
+        .setCouponCode(coupon_name);
 
-    afterEach : product_page
+    product_page
+        .clickApplyButton();    
+
+
+    client.pause(9000);
+
+    const couponAppliedNotification = `//div[@class='woocommerce-message']`;
+    client.useXpath();
+    client.assert.visible(couponAppliedNotification, 'UI: Coupon Applied Notification');  
+    client.useCss();
+
+    page
+        .navigate()
+
+    client.assert.visible(mainPageHeader, 'UI: Main Page Visible');  
+
+    },
+    "After API Delete Product and Code Coupon" : function (client) {
+    
+    const product_page = client.page.product();
+    product_page
         .deleteViaAPI();
 
-    // page
-    //     .navigate()
-
-    // client.assert.visible(mainPageHeader, 'UI: Main Page Visible');    
+    product_page
+        .deleteCuponViaAPI();
 
     }
 }
